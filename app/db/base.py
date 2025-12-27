94ef217cfe479dd4f487a64ba64025bf5e64079b
Pavira <pavivalavan@gmail.com>
2025-12-27 18:24:15 +0530

    update alembic

--- app/db/base.py
+++ app/db/base.py
@@ -2,4 +2,4 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-from app.models import worker_models
+from app.models import worker_models, company_model
--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app = FastAPI(
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
 app.include_router(worker.router, prefix="/api/v1/worker", tags=["Worker"])
 app.include_router(job.router, prefix="/api/v1/job", tags=["Job"])
+app.include_router(job.router, prefix="/api/v1/company", tags=["Company"])
 
 # Rate Limiting
 app.state.limiter = limiter
