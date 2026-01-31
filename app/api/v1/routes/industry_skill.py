075d36d8d88e85cebf0fc84d87d1921f039279ec
Pavira <pavivalavan@gmail.com>
2026-01-31 09:58:32 +0530

    Added get category skill and sub category api and service

--- app/api/v1/routes/industry_skill.py
+++ app/api/v1/routes/industry_skill.py
@@ -5,7 +5,11 @@ from app.db.session import get_db
 from app.core.limiter import limiter
 from app.utils.response import custom_response
 
-from app.services.industry_skill_service import get_industry_types_service
+from app.services.industry_skill_service import (
+    get_category_skills_service,
+    get_industry_types_service,
+    get_sub_category_skills_by_category_id_service,
+)
 
 
 router = APIRouter()
@@ -17,7 +21,7 @@ router = APIRouter()
     status_code=200,
 )
 @limiter.limit(
-    "15/minute"
+    "50/minute"
 )  # Allow only 5 requests per minute per IP, ex - requests/minute - 10/second
 def get_industry_types(
     request: Request,  # REQUIRED by SlowAPI
@@ -32,5 +36,61 @@ def get_industry_types(
         success=True,
         message="Industry types fetched successfully",
         data=industry_types,
+        code=status.HTTP_200_OK,
+    )
+
+
+# ------------------------End Get Industry Type Route ---------------------------------------------- #
+
+
+# -----------------------Get Category skills Route ----------------------- #
+@router.get(
+    "/category_skills",
+    status_code=200,
+)
+@limiter.limit(
+    "50/minute"
+)  # Allow only 5 requests per minute per IP, ex - requests/minute - 10/second
+def get_category_skills(
+    request: Request,  # REQUIRED by SlowAPI
+    db: Session = Depends(get_db),
+):
+    """
+    Get list of category skills.
+    """
+    category_skills = get_category_skills_service(db=db)
+    return custom_response(
+        success=True,
+        message="Category skills fetched successfully",
+        data=category_skills,
+        code=status.HTTP_200_OK,
+    )
+
+
+# ------------------------End Get Category skills Route ---------------------------------------------- #
+
+
+# -----------------------Get Sub-Category skills by category id Route ----------------------- #
+@router.get(
+    "/sub_category_skills/{category_skill_id}",
+    status_code=200,
+)
+@limiter.limit("50/minute")
+def get_sub_category_skills_by_category_id(
+    category_skill_id: str,
+    request: Request,  # REQUIRED by SlowAPI
+    db: Session = Depends(get_db),
+):
+    """
+    Get list of sub-category skills by category skill id.
+    """
+
+    sub_category_skills = get_sub_category_skills_by_category_id_service(
+        db=db, category_skill_id=category_skill_id
+    )
+    return custom_response(
+        success=True,
+        message="Sub-category skills fetched successfully",
+        data=sub_category_skills,
         code=status.HTTP_200_OK,
     )
--- app/services/industry_skill_service.py
+++ app/services/industry_skill_service.py
@@ -1,7 +1,11 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 
-from app.models.industry_skill_models import IndustryTypeModel
+from app.models.industry_skill_models import (
+    CategorySkillModel,
+    IndustryTypeModel,
+    SubCategorySkillModel,
+)
 
 
 # -----------------------Get Industry Type Service ----------------------- #
@@ -23,3 +27,56 @@ def get_industry_types_service(db: Session) -> list[IndustryTypeModel]:
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Error fetching industry types",
         )
+
+
+# ------------------------End Get Industry Type Service ---------------------------------------------- #
+# -----------------------Get Category Skills Service ----------------------- #
+def get_category_skills_service(db: Session) -> list[CategorySkillModel]:
+    """
+    Service to get list of category skills.
+    """
+    # put everthing in try except block
+    try:
+        category_skills = (
+            db.query(CategorySkillModel)
+            .filter(CategorySkillModel.is_active == True)
+            .all()
+        )
+        return category_skills
+    except Exception as e:
+        print("DB ERROR 👉", e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error fetching category skills",
+        )
+
+
+# ------------------------End Get Category Skills Service ---------------------------------------------- #
+
+
+# -----------------------Get sub Category skills by category id Service ----------------------- #
+def get_sub_category_skills_by_category_id_service(
+    db: Session, category_skill_id: str
+) -> list[SubCategorySkillModel]:
+    """
+    Service to get list of sub category skills by category id.
+    """
+    try:
+        sub_category_skills = (
+            db.query(SubCategorySkillModel)
+            .filter(
+                SubCategorySkillModel.category_skill_id == category_skill_id,
+                SubCategorySkillModel.is_active == True,
+            )
+            .all()
+        )
+        return sub_category_skills
+    except Exception as e:
+        print("DB ERROR 👉", e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error fetching sub category skills",
+        )
+
+
+# ------------------------End Get sub Category skills by category id Service ---------------------------------------------- #
